c3887a7f13aced21d293f3c7a8f6b6094accb800
Ignacio Armand-ugón <Matias4371@gmail.com>
2023-01-12 00:01:02 -0300

    Maquetada visualización de lista de productos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { Typography, Grid, Card, CardActionArea, CardMedia } from '@mui/material';
 import type { NextPage } from 'next';
 import { ShopLayout } from '../components/layouts';
+import { ProductList } from '../components/products';
 import { initialData } from '../database/products';
 
 const Home: NextPage = () => {
@@ -12,22 +13,7 @@ const Home: NextPage = () => {
       <Typography variant='h2' component={'h2'}>
         Todos los productos
       </Typography>
-
-      <Grid container spacing={4}>
-        {initialData.products.map((product) => (
-          <Grid item xs={6} sm={4} key={product.slug}>
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  component={'img'}
-                  image={`products/${product.images[0]}`}
-                  alt={product.title}
-                />
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+      <ProductList products={initialData.products as any} />
     </ShopLayout>
   );
 };
